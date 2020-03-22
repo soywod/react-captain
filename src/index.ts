1d7841c9c675030f2d6e91fbac7c6aa7e30d847e
Clément DOUIN <soywod@users.noreply.github.com>
2020-03-22 17:19:23 +0100

    update index imports

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import useClickOutside, {UseClickOutside, ClickOutsideFn, ClickOutsideOpts} from "./click-outside"
-import useDebounce, {UseDebounce, DebounceOpts, Debounce} from "./debounce"
-import useTimeout, {UseTimeout, TimeoutOpts, Timeout} from "./timeout"
-import useInterval, {UseInterval, IntervalFn, IntervalOpts} from "./interval"
-import useStoredState, {UseStoredState, StoredStateOpts} from "./stored-state"
-import useToggle, {UseToggle} from "./toggle"
-import useSubject, {UseSubject, SubjectFn} from "./subject"
-import useBehaviorSubject, {UseBehaviorSubject, BehaviorSubjectState} from "./behavior-subject"
-
-export {useClickOutside, UseClickOutside, ClickOutsideFn, ClickOutsideOpts}
-export {useDebounce, UseDebounce, DebounceOpts, Debounce}
-export {useTimeout, UseTimeout, TimeoutOpts, Timeout}
-export {useInterval, UseInterval, IntervalFn, IntervalOpts}
-export {useStoredState, UseStoredState, StoredStateOpts}
-export {useToggle, UseToggle}
-export {useSubject, UseSubject, SubjectFn}
-export {useBehaviorSubject, UseBehaviorSubject, BehaviorSubjectState}
+export {
+  default as useClickOutside,
+  UseClickOutside,
+  ClickOutsideFn,
+  ClickOutsideOpts,
+} from "./click-outside"
+export {default as useDebounce, UseDebounce, DebounceOpts, Debounce} from "./debounce"
+export {default as useTimeout, UseTimeout, TimeoutOpts, Timeout} from "./timeout"
+export {default as useInterval, UseInterval, IntervalFn, IntervalOpts} from "./interval"
+export {default as useStoredState, UseStoredState, StoredStateOpts} from "./stored-state"
+export {default as useToggle, UseToggle} from "./toggle"
+export {default as useSubject, UseSubject, SubjectFn} from "./subject"
+export {
+  default as useBehaviorSubject,
+  UseBehaviorSubject,
+  BehaviorSubjectState,
+} from "./behavior-subject"
